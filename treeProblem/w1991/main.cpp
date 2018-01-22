e56b2292bbbfeeab3a451051a08d2f7a35cb202c
snrndi121 <snrndi4521@gmail.com>
2018-01-22 16:16:11 +0900

    start tree */

---
+++ treeProblem/w1991/main.cpp
@@ -0,0 +1,18 @@
+/* preorder, inorder, postorder */
+
+#include <iostream>
+#include <vector>
+
+/* def */
+/* var */
+/* func */
+void input_read();
+void searching_tree();
+
+int main()
+{
+	input_read();
+	searching_tree();
+	return 0;
+}
+
