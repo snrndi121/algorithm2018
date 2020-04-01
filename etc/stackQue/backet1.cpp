12f96f93671c4e0e2ea05b8354c0de0286380d1c
snrndi121 <snrndi4521@gmail.com>
2020-04-01 16:42:56 +0900

    solve : stack problem, "check pairs of brackets"

--- etc/stackQue/backet1.cpp
+++ etc/stackQue/backet1.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stack>
+#include <vector>
 //def
 using namespace std;
 /*
@@ -34,23 +36,52 @@ using namespace std;
 // };
 // struct mstack < int > ms(100);
 //var
+uint sam_size = 0;
+vector < string > input_brackets;
 //func
-
 void read_input()
 {
-    for (int i = 0; i < 10; ++i) ms.push(i);
-    cout << ms.top() << endl;
-    for (int i = 0; i < 11; ++i) ms.pop();
-    cout << ms.top() << endl;
-
+    cin >> sam_size;
+    for (uint i = 0; i < sam_size; ++i) {
+        string s;
+        cin >> s;
+        input_brackets.push_back(s);
+    }
 }
-void write_output()
+void write_output(bool _res)
 {
-
+    if (_res) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+bool solve_bracket(const string& src)
+{
+    const string openings = "({[", closing = ")}]";
+    stack < char > remains;
+    for (int i = 0; i < src.size(); ++i) {
+        //시작인지 봐야하고
+        //시작인데 저 범위 안에 들어오는 지 확인도 같이 해줘야한다.
+        if (openings.find(src[i]) != -1)
+          //만약 두 조건이 맞다면
+          remains.push(src[i]);
+        //시작이 아니라면
+        else {
+          //top()이 일단 존재를 해야함. -> 혹은 마지막에 논리연산 곱 시켜줘도 되겠다.
+          if (remains.empty()) return false;
+          //짝이 맞는지 확인
+          if (openings.find(remains.top()) != closing.find(src[i])) return false;
+          remains.pop();
+        //아니라면 실패
+        }
+    }
+    //마지막에 남은 값이 있다면 false;
+    return remains.empty();
 }
 int main()
 {
     read_input();
-    write_output();
+    for (int i = 0; i < sam_size; ++ i) {
+      bool res = solve_bracket(input_brackets[i]);
+      write_output(res);
+    }
     return 0;
 }
